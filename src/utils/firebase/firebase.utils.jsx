cda95f3a9ee14af6a7dd32677fd09a298d8e43d3
Tafadzwa <musinachirevotafadzwa@gmail.com>
2023-09-04 12:53:00 +0200

    added sign in component

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -1,6 +1,7 @@
 
 import{initializeApp} from 'firebase/app';
 import {getAuth,signInWithRedirect,signInWithPopup,GoogleAuthProvider} from 'firebase/auth';
+import {getFirestore,doc,getDoc,setDoc} from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyCrHY8MpvrTmBcDpsLM6akvOjMnZ8xjbAU",
@@ -19,4 +20,26 @@ const firebaseConfig = {
 
   export const auth = getAuth();
   export const signInWithGooglePopup = ()=> signInWithPopup(auth,provider);
-  
+
+  export const db = getFirestore();
+   export const createUserDocumentFromAuth = async (userAuth)=>{
+     const userDocRef = doc(db,'users',userAuth.uid);
+
+     console.log(userDocRef)
+
+     const userSnapshot = await getDoc(userDocRef);
+     console.log(userSnapshot);
+     console.log(userSnapshot.exists())
+
+     if(!userSnapshot.exists()){
+      const{displayName, email} = userAuth;
+      const createdAt = new Date();
+      try{
+        await setDoc(userDocRef,{displayName,email,createdAt})
+      }
+      catch(error){
+        console.log('there was an eeror creating the user', error.message)
+      }
+     }
+     return userDocRef;
+  }
